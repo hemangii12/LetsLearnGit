916f0b70dfec383ab4bcfb5ea3022b7ebf283d4e
Hemangi Sarangdhar <hemangi21538@gmail.com>
2024-02-01 09:05:31 +0530

    Committed sum file

--- sum.java
+++ sum.java
@@ -3,6 +3,6 @@ public class sum{
         int a = 5;
         int b = 6;
         System.out.println(a+b);
-        System.out.println("Hello");
+        System.out.println("Hello from sum file");
     }
 }
